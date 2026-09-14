bba509b98706123e97a974a214e1cc74b4047dc4
RonTheRentalCop <YOUR_GITHUB_NOREPLY_EMAIL>
2026-09-14 08:22:57 -0700

    Tried to fix some minor colors and added some simple detection. Might change direction it sucks.

--- Object_Detection_Test.py
+++ Object_Detection_Test.py
@@ -1,5 +1,27 @@
 import time
 import cv2 as cv
+from ultralytics import YOLO
+
+
+model = YOLO("yolov8n.pt")
+
+
+def detect_objects(frame):
+    """Placeholder for the future YOLO object detection call."""
+    results = model(frame, verbose=False)
+    detections = []
+    for result in results:
+        for box in result.boxes:
+            x1, y1, x2, y2 = box.xyxy[0].tolist()
+            confidence = float(box.conf[0])
+            class_id = int(box.cls[0])
+            label = model.names[class_id]
+            detections.append({
+                "label": label,
+                "confidence": confidence,
+                "box": (int(x1), int(y1), int(x2), int(y2))
+            })
+    return detections
 
 
 camera = cv.VideoCapture(0)
@@ -15,6 +37,13 @@ while True:
 
     detected_objects = detect_objects(frame)
 
+    for obj in detected_objects:
+        x1, y1, x2, y2 = obj["box"]
+        label = obj["label"]
+        confidence = obj["confidence"]
+        cv.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+        cv.putText(frame, f"{label} {confidence:.2f}", (x1, y1 - 10), cv.FONT_HERSHEY_SIMPLEX, 0.5, (33, 172, 227, 0.169), 2)
+
     cv.imshow("Camera Feed", frame)
     if cv.waitKey(1) & 0xFF == ord('q'):
         break
@@ -22,16 +51,6 @@ while True:
 # Relase the camerea and boom the window
 camera.release()
 cv.destroyAllWindows()
-
-# Put this in a loop that continuously checks for detected objects and makes the car drive forward or stop and turn based on the detection results.
-def detect_objects(frame):
-    if detected_objects is None:
-        #Call Function that does not exist yet to make car drive forward
-        return []
-    elif detected_objects is not None:
-        detected_objects = []  # Placeholder for detected objects
-        # Call Function that does not exist yet to make car stop and turn
-        return detected_objects
 
 # The Big steps of this program 
 
@@ -44,20 +63,19 @@ def detect_objects(frame):
 # Have him hit things in range if they don't move ONLY when I make rage mode
 
 def loudspeakaer():
+    if detected_objects(person):
+        # Play sound on speaker Alien! Alien! Alien!
+        # If there are detected objects, make the drone angry
+        print("Detected objects! Drone is angry!")
+        # Add logic to make the drone angry here`
     # Don't know what the code will look like yet maybe use TTS
+    pass
 # This function will be called when the drone is in rage mode
 
 # Rage mode
-def rage_mode():
-    if detected_objects is not None:
-        print("RAGE. This Dirtlet is Mad ASF")
-        # Have the drone hit the objects. Maybe increase speed but first make it run an enviornmental check
-        # Cap the bvehicle speed to a certin metric for the safty of the solder and electronics inside
-        
-
-        # Call Function that does not exist yet to make car stop and turn
-        return detected_objects
+def Rage_Mode():
     # This function will be called when the drone is in rage mode
     # It will make the drone aggressive and hit objects in its path
 
     # Add logic to make the drone hit objects here
+    pass
